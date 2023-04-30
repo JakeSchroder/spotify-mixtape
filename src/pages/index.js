e75e61e834d964be2890bc54bb3def19d3845b4f
JakeSchroder <jake.schroder@gmail.com>
2023-04-30 04:33:52 -0500

    Init site pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Mixtape from './mixtape'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,8 +16,7 @@ export default function Home() {
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
       >
-        <div>
-        </div>
+        <Mixtape/>
       </main>
     </>
   )
