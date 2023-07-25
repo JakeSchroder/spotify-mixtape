13b29077fe2aebb6c5029df5b82072b7b220f8ac
JakeSchroder <jake.schroder@gmail.com>
2023-07-25 15:44:43 -0500

    Add Spotify Authorization Code with PKCE Flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
-import {Button} from '@chakra-ui/react';
-
+import SpotifyLogin from "./pkce_spotify_auth"
+import { getProfile, RequestAccessToken } from "./pkce_spotify_auth";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+    RequestAccessToken()
+    getProfile(localStorage.getItem('access_token'))
+  })
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Spotify Login</Button>
+      <button className='btn-primary' onClick={SpotifyLogin}>Spotify Login</button>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,48 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      text: '#000000',
+      background: '#fcf8fb',
+      'primary-button': '#847C44',
+      'secondary-button': '#c2c2c2',
+      accent: '#e7d982',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['var(--font-montserrat-alt)'],
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+      screens: {
+        sm: { max: '767px' },
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        md: { min: '768px', max: '1023px' },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+        lg: { min: '1024px', max: '1279px' },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+        xl: { min: '1280px', max: '1535px' },
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+        '2xl': { min: '1536px' },
+        // => @media (min-width: 1536px) { ... }
+      },
     },
   },
   plugins: [],
-}
+};
