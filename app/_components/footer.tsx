41dc2ad7e7268a06cb46c00a8c97cf906e028493
JakeSchroder <jake.schroder@gmail.com>
2023-07-31 01:08:46 -0500

    Add to content to footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,8 +1,13 @@
 export default function Footer(){
     return(
-        <div className=" flex flex-col items-center space-y-3" >
+        <div className=" flex flex-col items-center space-y-2 pb-5" >
             <hr className=" w-2/3 "/>
             <h3 className=" text-center">Made By Jake Schroder</h3>
+            <div className=" flex flex-row items-center space-x-3">
+                <a className=" btn-nav brightness-50" href="/">Home</a>
+                <a className=" btn-nav brightness-50" href="/about">About</a>
+                <a className=" btn-nav brightness-50" href="/privacy_policy">Privacy Policy</a>
+            </div>
         </div>
     )
 }
