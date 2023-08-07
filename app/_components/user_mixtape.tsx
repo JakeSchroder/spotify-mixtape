bb2ce865c03167fc60ff55e34fba4138b09cd27a
JakeSchroder <jake.schroder@gmail.com>
2023-08-07 17:48:14 -0500

    Remove JSON.parse

--- app/_components/user_mixtape.tsx
+++ app/_components/user_mixtape.tsx
@@ -11,11 +11,11 @@ interface DefaultProps {
 
 export default function UserMixtape(props:DefaultProps){
     return props.mixtapeTopTracks?.map((track:TrackProvider,index:number)=>{
-                    return(
-                        <div className='flex gap-2' key={index}>
-                            {props?.category =='tracks'? <h1>{track?.name} - {track?.artist}</h1>:<h1>{track?.artist}</h1>}
-                            <h1>| {track?.artist_popularity} followers</h1>
-                        </div>
-                    )
+                return(
+                    <div className='flex gap-2' key={index}>
+                        {props?.category =='tracks'? <h1>{track?.name} - {track?.artist}</h1>:<h1>{track?.artist}</h1>}
+                        <h1>| {track?.artist_popularity} followers</h1>
+                    </div>
+                )
             }) 
 }
